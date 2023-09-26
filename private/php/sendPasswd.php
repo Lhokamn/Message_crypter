50d162992828e879700cf0dca090d34208526266
corentin claudel <coren@PC-fixe-coco.constentisme.dom>
2023-09-26 19:38:17 +0200

    First commit

---
+++ private/php/sendPasswd.php
@@ -0,0 +1,6 @@
+<?php session_start();
+require_once('fonctions.php');
+$text=$_POST['passwdText'];
+header('location:../../web/Password_diffusion.php');
+?>
+
---
+++ web/Password_diffusion.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="../private/css/main.css">
+        <title>Secu Jimi</title>
+</head>
+<body>
+        <header>
+                <a href="index.html">
+                        <img src="../Assets/icone.png" alt="Logo Endrix">
+                </a>
+                <p>Secu JIMI - Osons nous protéger !</p>
+        </header>
+        <footer>
+                <p>En cas de problèmes, merci de contacter le support informatique sur <a href="https://jimi.endrix.com" target="_blank">Demande à Jimi</a></p>
+        </footer>
+</body>
+</html>
