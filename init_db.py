ca7125d105fcb6b2731f5c916317c9577ce8a9e2
Lhokam <corentin@cclaudel.fr>
2024-10-08 16:25:00 +0200

    init_db -> Rajout de tout un system d'exception en cas d'erreur lors du chargement du fichier schema.sql -> Les erreurs était du à la création d'un bd en mode mysql hérité de la dernière version. Passage en mode sqlite3

--- init_db.py
+++ init_db.py
@@ -1,10 +1,32 @@
 import sqlite3
+import os
+from dotenv import load_dotenv
 
-connection = sqlite3.connect('database.db')
+# Load .env file
+load_dotenv()
 
+database_URL = os.getenv('DATABASE_URL')
 
-with open('schema.sql') as f:
-    connection.executescript(f.read())
-    
-connection.commit()
-connection.close()
+if not database_URL:
+    raise ValueError("DATABASE_URL not found in the environment variables.")
+
+try :
+    connection = sqlite3.connect(database_URL)
+    print("Database connected successfully!")
+
+    # Execute SQL script from schema.sql
+    with open(file='./schema.sql', mode='r') as f:
+        connection.executescript(f.read())
+
+    # Commit changes
+    connection.commit()
+    print("Schema applied successfully!")
+
+except sqlite3.Error as e:
+    print(f"An error occurred while connecting to the database or executing the script: {e}")
+
+finally :
+    # Close the connection
+    if connection:
+        connection.close()
+        print("Database connection closed.")
--- schema.sql
+++ schema.sql
@@ -1,25 +1,8 @@
+DROP TABLE IF EXISTS passwdLinks;
 
-
-DROP TABLE IF EXISTS passwd;
-
--- Création de la base de donnée
-CREATE DATABASE passwd;
-
-
-CREATE TABLE passwd.Links (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    lien VARCHAR(8) UNIQUE NOT NULL,
-    texte VARCHAR(2048) NOT NULL,
-    usesLink BOOLEAN NOT NULL
+-- Create table for storing password links
+CREATE TABLE passwdLinks (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    lien TEXT UNIQUE NOT NULL,
+    texte TEXT NOT NULL
 );
-
-/** à régler pour plus tard
--- Création d'un utilisateur pour avoir un accès à la bd
-CREATE USER myUser@localhost IDENTIFIED BY 'AVy,!5AtET</E3A/h[]S';
-
--- Attribution des droits sur la base de donnée pour l'utilisateur
-GRANT INSERT,UPDATE ON passwd.* TO myUser@localhost;
-
--- Mise a jour des droits
-FLUSH PRIVILEGES;
-*/
