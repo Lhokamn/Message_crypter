cc223b2db7039fddf699aecd3cdbad15b5ae7a16
Lhokam <corentin@cclaudel.fr>
2024-10-09 22:02:55 +0200

    Add a condition to create db if database file doesn't exist

--- init_db.py
+++ init_db.py
@@ -10,23 +10,28 @@ database_URL = os.getenv('DATABASE_URL')
 if not database_URL:
     raise ValueError("DATABASE_URL not found in the environment variables.")
 
-try :
-    connection = sqlite3.connect(database_URL)
-    print("Database connected successfully!")
+# Because we use sqlite3 db, we are looking if file is already created, if it's not, we have to create it
 
-    # Execute SQL script from schema.sql
-    with open(file='./schema.sql', mode='r') as f:
-        connection.executescript(f.read())
+if not os.path.isfile(database_URL):
+    try :
+        connection = sqlite3.connect(database_URL)
+        print("Database connected successfully!")
 
-    # Commit changes
-    connection.commit()
-    print("Schema applied successfully!")
+        # Execute SQL script from schema.sql
+        with open(file='./schema.sql', mode='r') as f:
+            connection.executescript(f.read())
 
-except sqlite3.Error as e:
-    print(f"An error occurred while connecting to the database or executing the script: {e}")
+        # Commit changes
+        connection.commit()
+        print("Schema applied successfully!")
 
-finally :
-    # Close the connection
-    if connection:
-        connection.close()
-        print("Database connection closed.")
+    except sqlite3.Error as e:
+        print(f"An error occurred while connecting to the database or executing the script: {e}")
+
+    finally :
+        # Close the connection
+        if connection:
+            connection.close()
+            print("Database connection closed.")
+else:
+    print("Database already created")
