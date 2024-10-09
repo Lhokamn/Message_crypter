dd90d991b8649060ae8177b87940a4c7fa571076
Lhokam <corentin@cclaudel.fr>
2024-10-09 21:15:23 +0200

    404 error case solved

--- app.py
+++ app.py
@@ -73,5 +73,10 @@ def link_token(token):
 def about():
     return render_template('about.html')
 
+@app.errorhandler(404)
+def page_not_found(e):
+    # note that we set the 404 status explicitly
+    return render_template('404.html'), 404
+
 if __name__ == '__main__':
     app.run(debug = True)
